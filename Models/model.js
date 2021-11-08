3bce6849dfa603210338a77dfeb0d6b8179916e1
Lucas hunt <lucashunt@MacBook-Air-tilhrende-Lucas.local>
2021-11-08 20:22:43 +0100

    tilføjet mongoose mongodb

---
+++ Models/model.js
@@ -0,0 +1,23 @@
+//https://www.youtube.com/watch?v=bxsemcrY4gQ
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+
+}, {timestamps: true});
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog;
--- server.js
+++ server.js
@@ -1,10 +1,65 @@
+//henter express
 const express = require('express');
 const app = express();
 app.use(express.json());
 
-const port = 8080;
-app.listen(port, () => {
-    console.log(`server lytter på http://localhost:${port}`);
-});
+//bruger mongoose og henter model.js
+const mongoose = require('mongoose');
+const Blog = require('./Models/model')
 
-let test = 2;
+
+
+//connect til database på nettet
+const dbURI = 'mongodb+srv://lucashunt:jsp35hqe@dga.obhbf.mongodb.net/acme?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+ .then((result) => app.listen(3000) )
+ .catch((err) => console.log(err))
+
+
+ //sender til databasen, og databasen sender tilbage
+ app.get('/add-blog', (req,res) =>{
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) =>{
+            console.log(err);
+        });
+ })
+
+ //bruger find method til at sende alt fra databasen
+ app.get('/all-blogs', (req,res) =>{
+    Blog.find()
+        .then((result) =>{
+            res.send(result)
+        })
+        .catch((err) =>{
+            console.log(err);
+        });
+    
+ })
+//bruger findById method til at finde en blog
+app.get('/single-blog', (req,res) =>{
+    Blog.findById('618974c119b1e9cd69da0efe')
+        .then((result) =>{
+            res.send(result)
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
+})
+
+app.get('/blogs', (req,res) =>{
+    Blog.find()
+        .then((result) =>{
+
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
